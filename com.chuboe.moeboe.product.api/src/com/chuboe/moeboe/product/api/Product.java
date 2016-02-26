f3c16440acb644a5f3982f7df0b0fab8f5323847
Chuck Boecking <chuck@chuboe.com>
2016-02-26 11:48:43 -0600

    added product api

---
+++ com.chuboe.moeboe.product.api/src/com/chuboe/moeboe/product/api/Product.java
@@ -0,0 +1,17 @@
+package com.chuboe.moeboe.product.api;
+
+import java.util.List;
+
+/**
+ * 
+ */
+public interface Product {
+	
+	/**
+	 * 
+	 */
+	ProductDTO save(ProductDTO product) throws Exception;
+    ProductDTO find(String _id) throws Exception;
+    List<ProductDTO> list(String filter) throws Exception;
+    boolean delete(String _id) throws Exception; //TODO: needs to be a response object
+}
---
+++ com.chuboe.moeboe.product.api/src/com/chuboe/moeboe/product/api/ProductDTO.java
@@ -0,0 +1,11 @@
+package com.chuboe.moeboe.product.api;
+
+import org.osgi.dto.DTO;
+
+public class ProductDTO extends DTO {
+	public String _id;
+	public String name;
+	public String uom_id;
+	public long created;
+	public long updated;
+}
