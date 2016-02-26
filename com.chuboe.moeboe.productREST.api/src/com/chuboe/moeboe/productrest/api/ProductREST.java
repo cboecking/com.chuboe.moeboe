4b847bb3e51c0511facc7f5f56739b8fe86fae49
Chuck Boecking <chuck@chuboe.com>
2016-02-26 12:29:34 -0600

    created ProductREST api

---
+++ com.chuboe.moeboe.productREST.api/src/com/chuboe/moeboe/productrest/api/ProductREST.java
@@ -0,0 +1,27 @@
+package com.chuboe.moeboe.productrest.api;
+
+import java.util.List;
+
+import com.chuboe.moeboe.product.api.ProductDTO;
+
+import osgi.enroute.rest.api.RESTRequest;
+
+/**
+ * 
+ */
+public interface ProductREST {
+	
+	/**
+	 * 
+	 */
+	ProductDTO getProduct(String _id) throws Exception;
+	List<ProductDTO> getProducts(String filter) throws Exception;
+
+	interface ProductRequest extends RESTRequest {
+		ProductDTO _body();
+	}
+	ProductDTO postProduct(ProductRequest pr) throws Exception;
+
+	boolean deleteProduct(String _id) throws Exception;
+
+}
