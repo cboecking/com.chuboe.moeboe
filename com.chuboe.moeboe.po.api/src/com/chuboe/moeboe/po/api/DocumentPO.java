cb25edce3ab23acc89915e668977c8688d7264fb
Chuck Boecking <chuck@chuboe.com>
2016-03-02 16:41:47 -0600

    fixed DocumentPO constant error

--- com.chuboe.moeboe.po.api/src/com/chuboe/moeboe/po/api/DocumentPO.java
+++ com.chuboe.moeboe.po.api/src/com/chuboe/moeboe/po/api/DocumentPO.java
@@ -12,6 +12,6 @@ public interface DocumentPO<T extends RecordDTO> extends RecordPO<T> {
 	String DOCUMENTPO_ACTION_PREPARE = "com.chuboe.moeboe.documentpo.action.prepare";
 	String DOCUMENTPO_ACTION_COMPLETE = "com.chuboe.moeboe.documentpo.action.complete";
 	String DOCUMENTPO_ACTION_REVERSE_CORRECT = "com.chuboe.moeboe.documentpo.action.reverse_correct";
-	String DOCUMENTPO_ACTION_REVERSE_ACCRUE = "com.chuboe.moeboe.documentpo.action.accrue";
+	String DOCUMENTPO_ACTION_REVERSE_ACCRUE = "com.chuboe.moeboe.documentpo.action.reverse_accrue";
 	String DOCUMENTPO_ACTION_REVERSE_VOID = "com.chuboe.moeboe.documentpo.action.void";
 }
